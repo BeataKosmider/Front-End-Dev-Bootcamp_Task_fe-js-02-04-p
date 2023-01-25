412d1d293b156b7be5569fdd3633627c97c3b478
Beata Kosmider <kosmibe@gmail.com>
2023-01-25 13:40:19 +0100

    task is done

--- index.js
+++ index.js
@@ -1,4 +1,13 @@
 const compare = (a, b) => {
+  if (a > b) {
+    return 1;
+  }
+  if (a < b) {
+    return -1;
+  }
+  if ((a = b)) {
+    return 0;
+  }
   // Zaimplementuj zadanie zgodnie z wytycznymi
 };
 
